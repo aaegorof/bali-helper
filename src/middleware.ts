2fff1c7e4ac0df8c4d560218df3c77ce1b0a1fdb
Artemiy Egorov <artemiyegorov@hollandandbarrett.com>
2025-11-04 13:39:36 +0800

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,20 @@
 import { updateSession } from '@/app/lib/supabase/middleware';
 import { type NextRequest } from 'next/server';
-import { menuItems } from './components/menuItems';
 
 export async function middleware(request: NextRequest) {
   return await updateSession(request);
 }
 
-const pagematcher = menuItems.map((item) => item.authMatcher).filter(Boolean);
-const negativeMatcher = ['/((?!_next/static|_next/image|favicon.ico|.*\\.[^/]+$).*)'];
-const apiMatcher = ['/api/:path*'];
-
-const matcher = [...negativeMatcher, '/(?!login|auth|error).*', ...pagematcher, ...apiMatcher];
-
+// export const config = {
+//   matcher: matcher,
+// };
 export const config = {
-  matcher: matcher,
+  matcher: [
+    // Исключаем статические файлы
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.[^/]+$).*)',
+    // Явно указанные маршруты из menuItems
+    '/permata/:path*',
+    // API маршруты
+    '/api/:path*',
+  ],
 };
